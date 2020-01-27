2d4e03634b8b8b66e43de06c5439fa198d243eeb
Brad Jenner <bradjenner@Brads-MacBook-Pro-2.local>
2020-01-27 00:47:41 -0800

    init

---
+++ knotty.py
@@ -0,0 +1,162 @@
+######################
+#
+# Knotty 
+#
+######################
+import argparse
+import numpy as np
+
+class operations():
+
+	def triNormal(self.p1, p2, p3):
+
+	return np.cross((p2 - p1), (p3 - p1))
+
+
+	def triSideNormals(self, p1, p2, p3, N):
+
+		N_12 = np.cross((p2 - p1), N) 
+		N_23 = np.cross((p3 - p2), N)
+		N_31 = np.cross((p1 - p3), N)
+
+		return [N_12, N_23, N_31]
+
+
+	def plane_dist(self, p1, N):
+
+		return ( -1 * np.dot(N, p1) )
+
+
+	def distance(self, xp, tp, N_side):
+
+		return ( np.dot((xp - tp), N_side) / np.linalg.norm(N_side) )
+
+	def tri_intersection(self, points, side_normals):
+
+		for i in range(len(side_normals)):
+			p = points[i]
+			n = side_normals[i]
+			dist = operations.distance(R, p, n)
+
+			if dist > 0:
+				return 0
+			elif dist < 0 and i == 2:
+				return 1
+
+
+
+
+
+class knotFinder():
+
+	def __init__(self):
+		pass
+
+	def trefoil(self, p1, p2, p3, s1, s2):
+
+		N = operations.triNormal(p1, p2, p3)
+
+		points = [p1, p2, p3]
+
+		D = operations.plane_dist(p1, N)
+
+		Unit_N = N / np.linalg.norm(N)
+
+		t = ( np.dot((D + Unit_N), s1) ) / ( np.dot(Unit_N, (s2 - s1)) )
+
+		if t < 0 or t > 1:
+			print("No Intersection")
+			exit()
+
+		R = s1 + t * (s2 - s1)
+
+		Tri_Side_Normals = operations.triSideNormals(p1, p2, p3, N)
+
+		intersect = operations.tri_intersection(points, Tri_Side_Normals)
+
+		return 
+
+
+	def figeight(self, protein):
+
+		pass
+
+	def quinqefoil(self, protein):
+		
+		pass 
+
+	def pentaknot(self, protein):
+		
+		pass 
+
+	def scan(self, sequence):
+
+		iterations = 0
+
+		while iterations < 500:
+
+			for i in range(1, len(sequence) - 3):
+				seq_list = sequence[i-1:i+2]
+				aa_prime = list(map(lambda x : x/3, np.sum(seq_list, axis=0)))
+
+				prev_list = [sequence[i-1], sequence[i], aa_prime]
+				next_list = [sequence[i], aa_prime, sequence[i+1]]
+
+				knot_presence = False
+
+				for j in range(i + 2, len(sequence) - 1):
+					prev_tri = tuple(prev_list + sequence[j-1:j+1])
+					next_tri = tuple(next_list + sequence[j:j+2])
+					prev_knot = knotFinder.trefoil(*prev_tri)
+					next_knot = knotFinder.trefoil(*next_tri)
+
+					if prev_knot == 1 or next_knot == 1:
+						knot_presence = True
+
+				if knot_presence == True:
+					sequence[i] = aa_prime
+
+			iterations += 1
+
+
+class protein():
+
+	def __init__(self, InputFile):
+
+		with read("Input_File", "r") as fi:
+			lines = fi.readlines()
+		
+		self.backbone = list(map(lambda x : np.array(x.split('\t')), lines))
+
+
+############################################
+# Argument Parser 
+
+def main():
+	parser = argparse.ArgumentParser(description='Protein Knot Detection')
+	parser.add_argument('-i','--input', help='file of amino acid sequence coordinates', required=True)
+	parser.add_argument('-o','--output', help='name of output file. If not specified, default is given with overwrite capabilities', required=True)
+	args = vars(parser.parse_args())
+
+	try:
+		InputFile = args["input"]
+	except:
+		raise ValueError("*** No Input File Given ***")
+
+
+	try:
+		OutputFile = args["output"]
+	except:
+		OutputFile = "knotty_output.py"
+		
+
+	aa_chain = protein(InputFile)
+
+	result = knotFinder.scan(aa_chain.backbone)
+
+	with open("OutputFile", "w") as fo:
+		fo.write(result)
+
+
+if __name__ == '__main__':
+	main()
---
+++ test.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+# def triNormal(p1, p2, p3):
+
+# 	return np.cross((p2 - p1), (p3 - p1))
+
+
+# def triSideNormals(p1, p2, p3, N):
+
+# 	N_12 = np.cross((p2 - p1), N) 
+# 	N_23 = np.cross((p3 - p2), N)
+# 	N_31 = np.cross((p1 - p3), N)
+
+# 	return [N_12, N_23, N_31]
+
+
+# def plane_dist(p1, N):
+
+# 	return ( -1 * np.dot(N, p1) )
+
+
+# def distance(xp, tp, N_side):
+
+# 	return ( np.dot((xp - tp), N_side) / np.linalg.norm(N_side) )
+
+
+
+
+p1 = [1, 0, 0]
+p2 = [0, 1, 0]
+p3 = [0, 0, 1]
+
+s1 = [2, 2, 2]
+s2 = [1, 1, 1]
+
+p1 = np.array(p1)
+p2 = np.array(p2)
+p3 = np.array(p3)
+
+points = [p1, p2, p3]
+
+print(list(map(lambda x : x/3, np.sum(points, axis=0))))
+
+# s1 = np.array(s1)
+# s2 = np.array(s2)
+
+# points = [p1, p2, p3]
+
+# N = triNormal(p1, p2, p3)
+
+# D = plane_dist(p1, N)
+
+# Unit_N = N / np.linalg.norm(N)
+
+# t = ( np.dot((D + Unit_N), s1) ) / ( np.dot(Unit_N, (s2 - s1)) )
+
+# if t < 0 or t > 1:
+# 	print("No Intersection")
+# 	exit()
+
+# R = s1 + t * (s2 - s1)
+
+# Tri_Side_Normals = triSideNormals(p1, p2, p3, N)
+
+# for i in range(len(Tri_Side_Normals)):
+# 	p = points[i]
+# 	n = Tri_Side_Normals[i]
+# 	dist = distance(R, p, n)
+
+# 	if dist > 0:
+# 		print("No Intersection")
+# 	elif dist < 0 and i == 2:
+# 		print("Intersection")
+
+# def func(a, b, c, d, e):
+# 	print(a, b, c, d, e)
+
+# sequence = "ABCDEFGHIJK"
+# for i in range(1, len(sequence) - 3):
+# 	seq_list = sequence[i-1:i+2]
+# 	for j in range(i + 2, len(sequence) - 1):
+# 		seq = seq_list + sequence[j:j+2]
+# 		seq = tuple(seq_list + sequence[j:j+2])
+# 		func(*seq)
+
+
+
